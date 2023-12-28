a55a2919918bd352fa53a890555a13138047fa8c
Springrain53 <154506565+Springrain53@users.noreply.github.com>
2023-12-28 19:44:27 +0900

    Update associative-array.php

--- kadai_007/associative-array.php
+++ kadai_007/associative-array.php
@@ -1,32 +1,11 @@
-<body>
-    <html>
-        <p>
-            <?php
-                  $food_data = ['name' => 'onion', 'price' => 200, 'weight' => '160'];
-                  print_r($food_data);
-            ?>
-        </p>
-    </html>
-</body>
-
-
- <!DOCTYPE html>
- <html lang="ja">
- 
- <head>
-     <meta charset="UTF-8">
- </head>
- 
  <body>
      <p>
          <?php
          // 連想配列に値を代入する
-                $food_data = ['name' => 'onion', 'price' => 200, 'weight' => '160'];
+                $food_data = ["name" => "onion", 'price' => 200, 'weight' => '160'];
 
          // 連想配列の値を出力する
                 print_r($food_data);
          ?>
      </p>
  </body>
- 
- </html>
