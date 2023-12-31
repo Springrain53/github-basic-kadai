657e8441223ed21c7355d925d96a3153b55eea9f
Springrain53 <154506565+Springrain53@users.noreply.github.com>
2023-12-31 12:28:29 +0900

    Add files via upload

---
+++ kadai_011/foreach.php
@@ -0,0 +1,17 @@
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>PHP課題</title>
+</head>
+    <body>
+        <p>
+            <?php
+                  $food_data = ["name" => "玉ねぎ", 'price' => 200, "maid" => "北海道"];
+                  print_r($food_data);
+            ?>
+        </p>
+    
+    </body>
+</html>
