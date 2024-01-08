5abede0c12390c13322acf6640ba1bb473c229a6
Springrain53 <154506565+Springrain53@users.noreply.github.com>
2024-01-08 20:05:24 +0900

    Update foreach.php

--- kadai_011/foreach.php
+++ kadai_011/foreach.php
@@ -8,9 +8,9 @@
     <body>
         <p>
             <?php
-                  $food_data = ["name" => "玉ねぎ", 'price' => 200, "maid" => "北海道"];
-                  foreach($food_data as $result){
-                      echo $result;
+                  $food_data = ["名前" => "玉ねぎ", '値段' => 200, "産地" => "北海道"];
+                  foreach($food_data as $key => $value){
+                      echo $key . ':' . $value;
                       echo '<br>';
                   }
             ?>
