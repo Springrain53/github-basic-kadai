5bf96544026ef129ab0fc69439dcbe2dda8388e7
Springrain53 <154506565+Springrain53@users.noreply.github.com>
2024-01-08 18:56:23 +0900

    Update class.php

--- kadai_016/class.php
+++ kadai_016/class.php
@@ -1,34 +1,38 @@
-
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-    <meta charset="UTF-8">
-    <title>PHP課題</title>
-</head>
-    <body>
-        <p>
-            <?php
-            class Food {
-              public function __construct(string $name, int $price) {
-                $this->name = $name;
-                $this->price = $price;
-              }
-          }
-          class Animal {
-            public function __construct(string $name, int $height, int $weight) {
-              $this->name = $name;
-              $this->height = $height;
-              $this->weight = $weight;
-            }
-        }          
-
-        $apple = new Food('リンゴ',10);
-        $cat = new Animal('マロン',20,30);
-        print_r($apple);
-        echo '<br>';
-        print_r($cat);
-            ?>
-        </p>
+<?php
+class Food {
+    public function __construct(string $name, int $price) {
+        $this->name = $name;
+        $this->price = $price;
+    }
     
-    </body>
-</html>
+    public function show_price() {
+        return $this->price;
+    }
+}
+
+class Animal {
+    public function __construct(string $name, int $height, int $weight) {
+        $this->name = $name;
+        $this->height = $height;
+        $this->weight = $weight;
+    }
+    
+    public function show_height() {
+        return $this->height;
+    }
+}
+
+$apple = new Food('リンゴ', 10);
+$cat = new Animal('マロン', 20, 30);
+
+print_r($apple);
+echo '<br>';
+print_r($cat);
+echo '<br>';
+
+// priceを表示
+echo $apple->show_price(); 
+echo '<br>';
+// heightを表示
+echo $cat->show_height(); 
+?>
